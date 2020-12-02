2cb18175e9998c207f364585e642ef6df8ee187f
AJAY KUMAR DHYANI <66765596+dhyanio@users.noreply.github.com>
2020-12-02 14:24:54 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ set -x
 : "${GITHUB_REPOSITORY?GITHUB_REPOSITORY has to be set. Did you use the actions/checkout action?}"
 cd ${GITHUB_REPOSITORY}
 
-molecule ${INPUT_MOLECULE_OPTIONS} ${INPUT_MOLECULE_COMMAND} ${INPUT_MOLECULE_ARGS}
+molecule ${INPUT_MOLECULE_OPTIONS} ${INPUT_MOLECULE_COMMAND} ${INPUT_MOLECULE_ARGS} > file.txt && echo 'output<<EOF' >> $GITHUB_ENV && cat file.txt >> $GITHUB_ENV && echo 'EOF' >> $GITHUB_ENV
